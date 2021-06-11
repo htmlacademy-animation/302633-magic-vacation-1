ab1281078b69c9c620bfd944917dcce9ae927069
olga <olya-ef123@mail.ru>
2021-06-11 21:27:48 +0300

    add win title animation

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -12,11 +12,13 @@ export default () => {
         let targetEl = [].slice.call(results).filter(function (el) {
           return el.getAttribute(`id`) === target;
         });
-        setTimeout(() => targetEl[0].classList.add(`screen--show`, 0));
+        setTimeout(() => {
+          targetEl[0].classList.add(`screen--show`);
+          initAnimation(targetEl[0]);
+        }, 0);
         targetEl[0].classList.remove(`screen--hidden`);
       });
     }
-
     let playBtn = document.querySelector(`.js-play`);
     if (playBtn) {
       playBtn.addEventListener(`click`, function () {
@@ -30,3 +32,56 @@ export default () => {
     }
   }
 };
+
+const createSvgAnimationEl = (tag, attrs) => {
+  const el = document.createElementNS(`http://www.w3.org/2000/svg`, tag);
+
+  attrs.forEach((attr) => {
+    el.setAttribute(attr.name, attr.value)
+  });
+
+  return el;
+};
+
+const playVictoryAnimation = (animatedEl) => {
+  const pathEls = animatedEl.querySelectorAll(`path`);
+  animatedEl.setAttribute(`transform`, `scale(1.15 1.15)`);
+  const animateTransform =  createSvgAnimationEl(`animateTransform`, [
+    {name: `attributeName`, value: `transform`},
+    {name: `type`, value: `scale`},
+    {name: `values`, value: `1.15 1.15; 1 1`},
+    {name: `keyTimes`, value: `0; 1`},
+    {name: `dur`, value: `1s`},
+    {name: `fill`, value: `freeze`},
+    {name:  `begin`, value: `1s`},
+    {name:  `additive`, value: `sum`}
+  ]);
+
+  pathEls.forEach((path) => {
+    const totalLength = path.getTotalLength();
+    const length = totalLength / 3;
+    path.setAttribute(`stroke-dasharray`, `0, ${length}`);
+
+    const animate = createSvgAnimationEl(`animate`, [
+      {name: `attributeName`, value: `stroke-dasharray`},
+      {name: `from`, value: `0, ${length}`},
+      {name: `to`, value: `${length}, 0`},
+      {name: `dur`, value: `0.5s`},
+      {name: `fill`, value: `freeze`},
+      {name:  `begin`, value: `1.5s`}
+    ]);
+    path.appendChild(animate);
+  });
+
+  animatedEl.appendChild(animateTransform)
+};
+
+const initAnimation = (targetEl) => {
+  const targetElementId = targetEl.getAttribute(`id`);
+  const animatedEl = targetEl.querySelector(`.result__slogan`);
+
+  if (targetElementId !== `result3`) {
+    playVictoryAnimation(animatedEl)
+  }
+}
+
